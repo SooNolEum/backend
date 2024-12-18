45bc20088baa95ab5565814d5299a3d9b79e06d8
Hugh <lee2317884@gmail.com>
2024-12-18 20:19:19 +0900

    feat: Swagger 적용

--- src/main/java/com/soonoleum/education/controller/TestController.java
+++ src/main/java/com/soonoleum/education/controller/TestController.java
@@ -4,18 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soonoleum.education.repository.dto.request.TestRequest;
 import com.soonoleum.education.repository.dto.response.TestResponse;
 import com.soonoleum.education.service.TestService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Test API", description = "연결 테스트 컨트롤러 입니다.")
 public class TestController {
 
 	private final TestService testService;
